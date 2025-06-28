ac89b4f8e0b9be3c2bb90eb5ab82da7ae03a810a
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2025-06-28 20:21:34 +0600

    project initial setup

---
+++ src/layout/GeneralLayout.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/shared/Navbar.js";
+
+const GeneralLayout = () => {
+  return (
+    <div className="poppins-regular">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default GeneralLayout;
